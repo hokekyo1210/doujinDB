a819eec4732d8fc7ad12d620279a31b9d9b68977
t <t@113.35.171.177>
2016-03-27 05:43:54 +0900

    EXIT_ON_CLOSE! MenuBar update.

--- src/hokekyo1210/dojindb/ui/MainFrame.java
+++ src/hokekyo1210/dojindb/ui/MainFrame.java
@@ -1,10 +1,14 @@
 package hokekyo1210.dojindb.ui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
-public class MainFrame extends JFrame{
+public class MainFrame extends JFrame implements ActionListener{
 	
 	private static final int width = 630,
 							height = 638;
@@ -25,6 +29,7 @@ public class MainFrame extends JFrame{
 	
 	private void initFrame(){
 		this.setTitle(title);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(MainFrame.width, MainFrame.height);
 		this.setResizable(false);
 		this.setLocationRelativeTo(null);///‰æ–Ê‚Ì’†‰›‚ÉˆÚ“®
@@ -35,6 +40,9 @@ public class MainFrame extends JFrame{
 		/////JMenuBar’Ç‰Á
 		JMenuBar menuBar = new JMenuBar();
 		JMenu menu = new JMenu("File");
+		JMenuItem exitItem = new JMenuItem("Exit");
+		exitItem.addActionListener(this);
+		menu.add(exitItem);
 		menuBar.add(menu);
 		this.setJMenuBar(menuBar);
 		
@@ -44,5 +52,13 @@ public class MainFrame extends JFrame{
 		this.add(inputPanel);
 		this.add(dbPanel);
 		this.add(rightPanel);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		JMenuItem source = (JMenuItem) event.getSource();
+		if(source.getText().equals("Exit")){
+			System.exit(0);
+		}
 	}
 }
